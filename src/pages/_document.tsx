6ca8dbcf7d71303455b4d7a20a5a973b61256d98
MrShadowRIFAT <91284619+MrShadowRIFAT@users.noreply.github.com>
2025-05-15 23:40:06 +0600

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="description" content="OneDrive FTP Index" />
+          <meta property="og:image" content="https://github.com/MrShadowRIFAT/cloudflare-onedrive-index/blob/main/public/images/Meta%20Image%20Banner.png" />
+
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
