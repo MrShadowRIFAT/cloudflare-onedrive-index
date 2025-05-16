70f948fc402e9f6dd66de5e7714b1403ef724ca3
MrShadowRIFAT <91284619+MrShadowRIFAT@users.noreply.github.com>
2025-05-16 09:59:48 +0600

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,18 +19,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6977117001356450"
-     crossorigin="anonymous"></script>
-<!-- Horizontal Display Ad -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-6977117001356450"
-     data-ad-slot="5837595083"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
         </body>
       </Html>
     )
